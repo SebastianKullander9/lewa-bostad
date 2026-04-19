89a41c0170ebdd40944ca4460178ccc686ea0ed1
Sebastian Kullander <sebastian.kullander@protonmail.com>
2026-04-19 22:54:30 +0200

    add sliding gradient hover effect to buttons

--- components/ui/buttons/PrimaryButton.tsx
+++ components/ui/buttons/PrimaryButton.tsx
@@ -4,8 +4,14 @@
 
 export default function PrimaryButton() {
     return (
-        <div className="bg-background text-surface inline-block px-4 py-2 rounded-full">
+        <div className="bg-background text-surface inline-block px-4 py-2 rounded-full relative group overflow-hidden cursor-pointer">
             <p>Primär button</p>
+            <span
+                className="
+				absolute inset-0 translate-x-full group-hover:-translate-x-full transition-transform duration-550 
+				ease-in-out bg-linear-to-r from-transparent via-white/18 to-transparent
+				"
+            />
         </div>
     );
 }
--- components/ui/buttons/SecondaryButton.tsx
+++ components/ui/buttons/SecondaryButton.tsx
@@ -4,8 +4,14 @@
 
 export default function SecondaryButton() {
     return (
-        <div className="inline-block border border-background px-4 py-2 rounded-full">
+        <div className="relative inline-block border border-background px-4 py-2 rounded-full group overflow-hidden cursor-pointer">
             <p>Sekundär button</p>
+            <span
+                className="
+				absolute inset-0 translate-x-full group-hover:-translate-x-full transition-transform duration-550
+				ease-in-out bg-linear-to-r from-transparent via-white/50 to-transparent
+				"
+            />
         </div>
     );
 }
